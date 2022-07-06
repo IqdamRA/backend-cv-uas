dc889c5c90d8245b515523f4e85263303fcc0772
IqdamRA <rasifalfarobi@gmail.com>
2022-07-06 02:34:37 -0700

    backend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const { MongoClient } = require('mongodb');
 const app = express ();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const url = 'mongodb+srv://iqdam:rasif@database.uonpr.mongodb.net/test';
 const client = new MongoClient(url);
